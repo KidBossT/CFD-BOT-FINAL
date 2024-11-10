6ab9fd8058cc8b15dc7f62d2b6b7a54428712855
KidBossT <bubblewraaaap@gmail.com>
2024-11-10 20:33:43 +0530

    fix: set fixed port to 8080

--- backend/app.py
+++ backend/app.py
@@ -164,5 +164,4 @@ def conduct_cfd_analysis(image):
     }
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
